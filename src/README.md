b004194fe931fced70fa7d9e6a90e69adcbd149c
Doster <191287000@qq.com>
2017-08-17 19:17:21 +0800

    VUE

---
+++ src/README.md
@@ -0,0 +1,40 @@
+# Vue.js 第6天学习笔记
+
+## 知识点
+
+- webpack
+- webpack + vue + vue-router 构建豆瓣电影
+- vue-devtools 开发调试工具
+- 组件通信
+  + 父子组件通信
+    * props down
+    * events up
+  + 非父子组件通信
+    * event bus
+- vuex 状态管理模式
+- Promise
+- 改造博学谷案例为单页面应用程序
+- Vue 相关第三方生态库
+  + element-ui
+  + mint-ui
+  + iView
+- 优化及部署 Vue 程序
+
+## 反馈
+
+## 复习
+
+## 上午复习
+
+## 下午复习
+
+## 总结
+
+## 目标
+
+
+## 其他
+
+- 如何高效编写静态页
+- --save-dev 和 --save 的区别
+- 个人博客
---
+++ src/components/swipeList.vue
@@ -0,0 +1,52 @@
+﻿<template>
+	<div class="list">
+		<swiper :options="swiperOption" >
+			<swiper-slide v-for="item in imgArr">
+				<img class="photo" :src="item.src"/>
+			</swiper-slide>
+			<div class="swiper-pagination" slot="pagination"></div>
+		</swiper>
+	</div>	
+</template>
+
+<script>
+	import { swiper, swiperSlide } from 'vue-awesome-swiper'
+	const imgArr = [{
+		src:'/static/img/share_img.png'
+	},{
+		src:'/static/img/event_img.png'
+	},{
+		src:'/static/img/family_img.png'
+	},{
+		src:'/static/img/active_img.jpg'
+	}];
+	export default {
+		name: 'swipeList',
+		components: {
+		swiper,
+		swiperSlide
+		},
+		data() {
+			return {
+				swiperOption: {
+					slidesPerView: 4,
+					spaceBetween: 10,
+				},
+				imgArr: imgArr
+			}
+		}
+  }
+</script>
+
+<style lang='scss'>
+	.list {
+		width:100%;
+		display:block;
+  	margin-bottom: -10px;
+		
+		.photo {
+			height: 210px;
+			}
+	}
+
+</style>
